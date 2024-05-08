d54266cc20c4a8fa8ece36302bc9e401453e657e
PritishWa <92771064+PritishWa@users.noreply.github.com>
2024-05-08 17:29:31 +0530

    Task:create a file structure

---
+++ src/app_navigation/App_Drawer_Navigation.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App_Drawer_Navigation = () => {
+  return (
+    <View>
+      <Text>App_Drawer_Navigation</Text>
+    </View>
+  )
+}
+
+export default App_Drawer_Navigation
