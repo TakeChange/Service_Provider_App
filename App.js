f336b2217e4840b0180a3ece427bccd3beedbd80
Anuja Bhalerao <anujabhaleraoo69@gmail.com>
2024-05-09 17:22:45 +0530

    pull code

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 
-
 const App = () => {
   return (
     <View>
       <Text>App</Text>
     </View>
+   
+  
   )
 }
 
