68ad6dccfa98128ea2449e018ec45ba58132898a
vaishnavi9491 <vaishnavijadhav5279@gmail.com>
2024-05-09 20:29:51 +0530

    pull profile code

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import ChangePasswordScreen from './src/screen/ChangePasswordScreen'
-import ProfileScreen from './src/screen/ProfileScreen'
+
 
 
 const App = () => {
