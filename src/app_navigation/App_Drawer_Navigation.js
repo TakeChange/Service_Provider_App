52ef044bf77240eb13bb7e2a738d004d8709f973
Muskanshah29 <shahmuskan2907@gmail.com>
2024-05-10 14:09:24 +0530

    pull cahnges

--- src/app_navigation/App_Drawer_Navigation.js
+++ src/app_navigation/App_Drawer_Navigation.js
@@ -1,27 +1,4 @@
 
-
-// import React from 'react';
-// import { View, Text } from 'react-native'; // Ensure these imports are correct
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import { NavigationContainer } from '@react-navigation/native';
-// import HomeScreen from '../screen/home_module/HomeScreen'; // Verify the path to HomeScreen
-// import { SafeAreaProvider } from 'react-native-safe-area-context';
-// import BottomTabNavigation from './BottomTabNavigation';
-
-// const Drawer = createDrawerNavigator();
-
-// const App_Drawer_Navigation = () => {
-//   return (
-//       <Drawer.Navigator >
-//         <Drawer.Screen name="Home" component={BottomTabNavigation} />
-//       </Drawer.Navigator>
-
-//   );
-// }
-
-// export default App_Drawer_Navigation;
-
-// App_Drawer_Navigation.js
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import BottomTabNavigation from './BottomTabNavigation';
