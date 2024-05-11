091eddd8aad03e6ed59df7220c30f8ffbd90cae6
vaishnavi9491 <vaishnavijadhav5279@gmail.com>
2024-05-11 14:26:59 +0530

    pull changes

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import AppNavigation from './src/app_navigation/AppNavigation'
-import {NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
+
 
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    
-  <NavigationContainer>
-    <AppNavigation/>
-  </NavigationContainer>
-=======
     <NavigationContainer>
-      <AppNavigation/>
+      <AppNavigation />
     </NavigationContainer>
-    
->>>>>>> 706ff34bc791f83fe4af806afa61a8491baaaa41
+
+
   )
 }
 
