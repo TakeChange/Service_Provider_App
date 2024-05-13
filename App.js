7312cc3e93abe8d7847295a47321a5d872d3a637
vaishnavi9491 <vaishnavijadhav5279@gmail.com>
2024-05-13 10:18:06 +0530

    pull changes profile screen

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import AppNavigation from './src/app_navigation/AppNavigation'
 import { NavigationContainer } from '@react-navigation/native'
 
 
+
 const App = () => {
   return (
     <NavigationContainer>
       <AppNavigation />
     </NavigationContainer>
+
 
   )
 }
