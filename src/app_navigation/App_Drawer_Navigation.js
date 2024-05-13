4006d7c11b9c72a6877bbee95b5adafc58655470
manishashirsath2001 <manishashirsath0204@gmail.com>
2024-05-13 10:36:53 +0530

    forgot password screen

--- src/app_navigation/App_Drawer_Navigation.js
+++ src/app_navigation/App_Drawer_Navigation.js
@@ -3,12 +3,7 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import BottomTabNavigation from './BottomTabNavigation';
 import ForgotPassword from '../screen/authentication_module/ForgotPassword';
-<<<<<<< HEAD
 
-=======
-import CustomDrawer from './CustomDrawer';
-const Drawer = createDrawerNavigator();
->>>>>>> c0c89d7d3f3c5846aabeb8535c11bd3b504d7469
 
 const App_Drawer_Navigation = () => {
 
@@ -23,13 +18,13 @@ const App_Drawer_Navigation = () => {
           backgroundColor: 'transparent',
           elevation: 0,
           shadowOpacity: 0,
-          headerTintColor: 'pink',
+
         },
 
         headerTitle: ''
       }}
-      drawerContent={props => <CustomDrawer {...props} />}>
 
+    >
       <Drawer.Screen name="Home" component={BottomTabNavigation} />
       <Drawer.Screen name="ForgotPassword" component={ForgotPassword} />
     </Drawer.Navigator>
