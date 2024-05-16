e17c0b520b994e3e84452532e0af7718e5a87201
Tanujamj <tanujajadhav2612@gmail.com>
2024-05-16 21:19:41 +0530

    pull code

--- App.js
+++ App.js
@@ -2,10 +2,6 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import AppNavigation from './src/app_navigation/AppNavigation'
 import { NavigationContainer } from '@react-navigation/native'
-import Welcome from './src/screen/authentication_module/WelcomeScreen'
-
-
-
 
 const App = () => {
   return (
