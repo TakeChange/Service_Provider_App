74971a7d7640de05a5f6687c297b3983702fa2fb
manishashirsath2001 <manishashirsath0204@gmail.com>
2024-05-25 12:50:29 +0530

    Pull Changes

--- src/screen/authentication_module/SignInScreen.js
+++ src/screen/authentication_module/SignInScreen.js
@@ -2,59 +2,40 @@ import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from 'reac
 import React, { useState } from 'react'
 import { useFocusEffect } from '@react-navigation/native';
 
-const SignInScreen = ({navigation}) => {
+const SignInScreen = ({ navigation }) => {
 
   const [mobile, setMobile] = useState('');
   const [mobileError, setMobileError] = useState('');
- 
 
   const Validation = () => {
     var isValid = true;
     if (mobile == '') {
       setMobileError('Mobile Number do not empty');
-        isValid = false;
+      isValid = false;
     } else {
       setMobileError('');
     }
     const phoneNumberRegex = /^[0-9]{10}$/;
-        if (!mobile || !phoneNumberRegex.test(mobile)) {
-            setMobileError('Enter a valid 10-digit mobile number');
-            isValid = false;
-        } else {
-            setMobileError('');
-        }
-   
-    if (isValid) {
-       
+    if (!mobile || !phoneNumberRegex.test(mobile)) {
+      setMobileError('Enter a valid 10-digit mobile number');
+      isValid = false;
+    } else {
+      setMobileError('');
     }
-  
-}
-// const check = async () => {
-//   try {
-//       var mobile = await AsyncStorage.getItem('mobile');
 
-//       if (mobile == mobile) {
-//           ToastAndroid.show(' Successfully', ToastAndroid.LONG);
-//           await AsyncStorage.setItem('login', 'yes');
-//           navigation.navigate(OtpVerifyScreen)
-//       }
-//       else {
-//           ToastAndroid.show('Mobile number do not match', ToastAndroid.LONG);
-//       }
-//   }
-//   catch (e) {
-//       console.log(e)
-//   }
-// }
+    if (isValid) {
 
-useFocusEffect(
-  React.useCallback(() => {
+    }
+  }
+
+  useFocusEffect(
+    React.useCallback(() => {
       return () => {
-          // Reset errors when navigating away from screen
-          setMobileError('');
+        // Reset errors when navigating away from screen
+        setMobileError('');
       };
-  }, [])
-);
+    }, [])
+  );
   return (
     <View style={styles.container}>
 
@@ -73,26 +54,26 @@ useFocusEffect(
 
       <Text style={styles.textname}>Mobile Number</Text>
       <View style={styles.txtinput}>
-      <TextInput
-      style={styles.textfield}
+        <TextInput
+          style={styles.textfield}
           placeholder="Enter mobile Number"
           keyboardType='numeric'
           maxLength={10}
-      />
+        />
       </View>
       <Text style={styles.error}>{mobileError}</Text>
-      <TouchableOpacity style={styles.button} 
-      onPress={() => navigation.navigate('OtpVerifyScreen')}
+      <TouchableOpacity style={styles.button}
+        onPress={() => navigation.navigate('OtpVerifyScreen')}
       // onPress={Validation}
       >
         <Text style={styles.buttonText}>Get OTP</Text>
       </TouchableOpacity>
 
       <View style={styles.bottomtxt}>
-      <Text style={styles.txtname1}>New User?</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('SignUpScreen')}>
-      <Text style={styles.txtname2}> Create an Account</Text>
-      </TouchableOpacity>
+        <Text style={styles.txtname1}>New User?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('SignUpScreen')}>
+          <Text style={styles.txtname2}> Create an Account</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
@@ -108,14 +89,14 @@ const styles = StyleSheet.create({
   },
   title: {
     alignItems: 'center',
-    marginTop:'3%'
+    marginTop: '3%'
   },
   text: {
     fontSize: 20,
     fontWeight: 'bold',
     color: '#009eb4',
-   padding:'2%',
-   marginTop:'5%'
+    padding: '2%',
+    marginTop: '5%'
   },
   imgContainer: {
     justifyContent: 'center',
@@ -123,19 +104,19 @@ const styles = StyleSheet.create({
 
   },
   msg: {
-    color:'#000',
+    color: '#000',
     fontSize: 18,
     fontWeight: 'bold',
-    marginTop:'5%'
+    marginTop: '5%'
   },
-  msg1:{
-    color:'#000',
+  msg1: {
+    color: '#000',
     fontSize: 18,
     fontWeight: 'bold',
-    
+
   },
   textname: {
-    color:'#000',
+    color: '#000',
     fontWeight: '600',
     marginTop: '10%',
     fontSize: 15,
@@ -143,7 +124,7 @@ const styles = StyleSheet.create({
 
   },
   txtinput: {
-    height:56,
+    height: 56,
     color: '#000',
     backgroundColor: '#ffffff',
     fontSize: 15,
@@ -153,17 +134,17 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowRadius: 4,
     elevation: 5,
-    justifyContent:'center'
+    justifyContent: 'center'
 
   },
   textfield: {
     marginLeft: '2%',
-    color:'#000',
+    color: '#000',
   },
   button: {
     height: 56,
     backgroundColor: '#009eb4',
-     justifyContent: 'center',
+    justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 17,
     marginTop: '5%',
@@ -172,27 +153,27 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 15,
     fontWeight: 'bold',
-    
+
 
   },
-  txtname1:{
-    color:'#000',
-    fontSize:15,
-    marginLeft:30,
-    fontWeight:'bold'
+  txtname1: {
+    color: '#000',
+    fontSize: 15,
+    marginLeft: 30,
+    fontWeight: 'bold'
   },
-  txtname2:{
-    fontSize:15,
-   color:'#009eb4',
-   fontWeight:'bold'
+  txtname2: {
+    fontSize: 15,
+    color: '#009eb4',
+    fontWeight: 'bold'
   },
-  bottomtxt:{
-    flex:1,
-    flexDirection:'row',
-    marginTop:'2%',
-    
+  bottomtxt: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: '2%',
+
   },
-  error:{
+  error: {
     color: 'red',
     marginHorizontal: 10,
     marginTop: '1%'
