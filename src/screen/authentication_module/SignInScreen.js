a7bc76bdd143b158beb0d7b4c8aa6e17d6a98714
vaishnavi9491 <vaishnavijadhav5279@gmail.com>
2024-05-25 15:47:37 +0530

    pull code

--- src/screen/authentication_module/SignInScreen.js
+++ src/screen/authentication_module/SignInScreen.js
@@ -1,44 +1,27 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from 'react-native'
-import React, { useState } from 'react'
-import { useFocusEffect } from '@react-navigation/native';
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
 
 const SignInScreen = ({ navigation }) => {
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const [mobile, setMobile] = useState('');
-  const [mobileError, setMobileError] = useState('');
+  const handleGetOTP = () => {
 
-  const Validation = () => {
-    var isValid = true;
-    if (mobile == '') {
-      setMobileError('Mobile Number do not empty');
-      isValid = false;
-    } else {
-      setMobileError('');
+    const mobileNumberPattern = /^[0-9]{10}$/;
+    if (mobileNumberPattern.test(mobileNumber)) {
+
+      setErrorMessage('');
+      navigation.navigate('OtpVerifyScreen');
     }
-    const phoneNumberRegex = /^[0-9]{10}$/;
-    if (!mobile || !phoneNumberRegex.test(mobile)) {
-      setMobileError('Enter a valid 10-digit mobile number');
-      isValid = false;
-    } else {
-      setMobileError('');
+    else {
+
+      setErrorMessage('Please enter a valid 10-digit mobile number.');
     }
+  };
 
-    if (isValid) {
-
-    }
-  }
-
-  useFocusEffect(
-    React.useCallback(() => {
-      return () => {
-        // Reset errors when navigating away from screen
-        setMobileError('');
-      };
-    }, [])
-  );
   return (
     <View style={styles.container}>
-
       <View style={styles.imgContainer}>
         <Image
           source={require('../../asset/images/Signup.png')}
@@ -47,25 +30,23 @@ const SignInScreen = ({ navigation }) => {
       </View>
       <View style={styles.title}>
         <Text style={styles.text}>Login Here</Text>
-        <Text style={styles.msg}>Enter Your Mobile Number </Text>
-        <Text style={styles.msg1}>reset your password </Text>
-
+        <Text style={styles.msg}>Enter Your Mobile Number</Text>
+        <Text style={styles.msg1}>reset your password</Text>
       </View>
 
       <Text style={styles.textname}>Mobile Number</Text>
       <View style={styles.txtinput}>
         <TextInput
           style={styles.textfield}
-          placeholder="Enter mobile Number"
+          placeholder="Enter your Mobile number"
           keyboardType='numeric'
           maxLength={10}
+          value={mobileNumber}
+          onChangeText={setMobileNumber}
         />
       </View>
-      <Text style={styles.error}>{mobileError}</Text>
-      <TouchableOpacity style={styles.button}
-        onPress={() => navigation.navigate('OtpVerifyScreen')}
-      // onPress={Validation}
-      >
+      {errorMessage !== '' && <Text style={styles.error}>{errorMessage}</Text>}
+      <TouchableOpacity style={styles.button} onPress={handleGetOTP}>
         <Text style={styles.buttonText}>Get OTP</Text>
       </TouchableOpacity>
 
@@ -76,27 +57,26 @@ const SignInScreen = ({ navigation }) => {
         </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default SignInScreen
+export default SignInScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: '8%',
-
   },
   title: {
     alignItems: 'center',
-    marginTop: '3%'
+    marginTop: '3%',
   },
   text: {
     fontSize: 20,
     fontWeight: 'bold',
     color: '#009eb4',
     padding: '2%',
-    marginTop: '5%'
+    marginTop: '5%',
   },
   imgContainer: {
     justifyContent: 'center',
@@ -104,77 +84,72 @@ const styles = StyleSheet.create({
   },
   msg: {
     color: '#000',
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: '5%'
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: '5%',
   },
   msg1: {
     color: '#000',
-    fontSize: 18,
-    fontWeight: 'bold',
-
+    fontSize: 20,
+    fontWeight: '600',
   },
   textname: {
     color: '#000',
     fontWeight: '600',
     marginTop: '10%',
     fontSize: 15,
-    padding: '2%'
-
+    padding: '2%',
   },
   txtinput: {
     height: 56,
-    color: '#000',
+    color: '#000000',
+    marginTop: 5,
     backgroundColor: '#ffffff',
     fontSize: 15,
-    borderRadius: 17,
+    borderRadius: 10,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 4,
     elevation: 5,
-    justifyContent: 'center'
-
+    justifyContent:'center',
+    
   },
   textfield: {
     marginLeft: '2%',
     color: '#000',
+   
+  },
+  error: {
+    color: 'red',
+    marginTop: 5,
   },
   button: {
     height: 56,
     backgroundColor: '#009eb4',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 17,
-    marginTop: '5%',
+    borderRadius: 10,
+    marginTop: '10%',
   },
   buttonText: {
     color: '#fff',
     fontSize: 15,
     fontWeight: 'bold',
-
-
   },
   txtname1: {
-    color: '#000',
     fontSize: 15,
     marginLeft: 30,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   txtname2: {
     fontSize: 15,
     color: '#009eb4',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   bottomtxt: {
     flex: 1,
     flexDirection: 'row',
     marginTop: '2%',
-
   },
-  error: {
-    color: 'red',
-    marginHorizontal: 10,
-    marginTop: '1%'
-  }
-})
+});
