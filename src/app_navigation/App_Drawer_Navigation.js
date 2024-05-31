64d3805086c14978f1dab9295623126221018614
vaishnavi9491 <vaishnavijadhav5279@gmail.com>
2024-05-31 12:37:11 +0530

    pull drawer code

--- src/app_navigation/App_Drawer_Navigation.js
+++ src/app_navigation/App_Drawer_Navigation.js
@@ -4,6 +4,8 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import BottomTabNavigation from './BottomTabNavigation';
 import FavouriteService from '../screen/services/FavouriteService';
 import CustomDrawer from '../app_navigation/CustomDrawer'
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 const App_Drawer_Navigation = () => {
 
   const Drawer = createDrawerNavigator();
@@ -23,8 +25,32 @@ const App_Drawer_Navigation = () => {
       }}
       drawerContent={props => <CustomDrawer {...props} />}
     >
-      <Drawer.Screen name="Home" component={BottomTabNavigation} />
-      <Drawer.Screen name="FavouriteService" component={FavouriteService} />
+
+      <Drawer.Screen
+        name="Home"
+        component={BottomTabNavigation}
+        options={{
+          headerShown: true,
+          title: 'Home',
+          drawerIcon: ({ focused, size }) => (
+            <FontAwesome name='home' size={22} color='#000'
+              style={{ height: 25, width: 25, marginRight: -20 }} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="FavouriteService"
+        component={FavouriteService}
+        options={{
+          headerShown: true,
+          title: 'Favourite Service',
+          drawerIcon: ({ focused, size }) => (
+            <MaterialIcons name='favorite'
+              size={22} color='#000'
+              style={{ height: 25, width: 25, marginRight: -20 }} />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
